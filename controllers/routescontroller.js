1f50334b180d25e8ebabd66596809673d8f813cc
mwomack <mikewomack2000@yahoo.com>
2018-12-13 17:54:50 -0800

    Styling added to saved page. Saving and notes still needed

--- controllers/routescontroller.js
+++ controllers/routescontroller.js
@@ -1,4 +1,8 @@
 var express = require("express");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("../models");
@@ -23,7 +27,7 @@ router.get("/", function (req, res) {
 // Route for grabbing saved articles
 router.get("/saved", function (req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({})
+  db.Article.find({ saved: true })
     .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.render("saved", { items: dbArticle });
@@ -34,13 +38,27 @@ router.get("/saved", function (req, res) {
     });
 });
 
+/// NEED TO FIGURE out how to change {saved: true}
+// router.post("/saved/:id", function (req, res) {
+//   // Grab every document in the Articles collection
+//   db.Article.find({ saved: true })
+//     .then(function (dbArticle) {
+//       return db.Article.findOneAndUpdate({ _id: req.params.id }) 
+//       // If we were able to successfully find Articles, send them back to the client
+//       // res.render("saved", { items: dbArticle });
+//     })
+//     .catch(function (err) {
+//       // If an error occurred, send it to the client
+//       res.json(err);
+//     });
+// });
+
 // A GET route for scraping the buzzfeed website
 router.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
   axios.get("https://www.buzzfeed.com/").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-    // var resultArray = [];
 
     // Now, we grab every h2 within a 'link-gray' class, and do the following:
     $("h2.link-gray").each(function (i, element) {
@@ -57,16 +75,16 @@ router.get("/scrape", function (req, res) {
         .parents("a")
         .attr("href");
 
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function (dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function (err) {
-            // If an error occurred, send it to the client
-            return res.json(err);
-          });
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
       // var article = new db.Article(result)
       // article.save();
     });
@@ -76,21 +94,6 @@ router.get("/scrape", function (req, res) {
     res.send("Scrape Complete");
   });
 });
-
-// Route for getting all Articles from the db
-router.get("/articles", function (req, res) {
-  // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function (dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
 
 // Clear data
 router.get('/clear', function (req, res) {
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,15 +1,46 @@
 
 
-$(".clear").on("click", function(req, res) {
+$(".clear").on("click", function (req, res) {
   console.log("button working")
 
   $.ajax({
     method: "GET",
     url: "/clear"
   })
-  .then(function(data) {
-    window.location.reload();
-  })
-  
+    .then(function (data) {
+      window.location.reload();
+    })
+
 })
+
+$(".scrape-new").on("click", function (req, res) {
+  console.log("button working")
+
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
+  })
+    .then(function (data) {
+      window.location.reload();
+    })
+
+})
+
+/// NEED TO FIGURE out how to change {saved: true}
+// $(".save-btn").on("click", function () {
+//   var thisId = $(this).attr("data-id");
+//   console.log("button test")
+
+//   $.ajax({
+//     method: "POST",
+//     url: "/saved",
+//     data: {
+//       saved: true
+//     }
+//   })
+//     .then(function (data) {
+//       console.log(data);
+//     })
+
+// })
 
